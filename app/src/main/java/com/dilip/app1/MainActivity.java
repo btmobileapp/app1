73b237e57f6eaeb04090e3f9e150e0bbd189db36
BT-18 <btmobileapp@gmail.com>
2019-11-11 11:02:37 +0530

    Second Push

--- app/src/main/java/com/dilip/app1/MainActivity.java
+++ app/src/main/java/com/dilip/app1/MainActivity.java
@@ -2,12 +2,21 @@ package com.dilip.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
